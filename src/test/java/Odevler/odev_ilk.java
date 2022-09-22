83e8a9cc03c5ef06902c8d84c75e217faf7abefc
yasin <y.mesekoparan1510@gmail.com>
2022-09-22 23:24:44 +0300

    22.09

--- src/test/java/Odevler/odev_ilk.java
+++ src/test/java/Odevler/odev_ilk.java
@@ -35,4 +35,40 @@ public class odev_ilk {
     //5-üçüncü ürüne relative locater kullanarak tıklayin
     //6-title ve fiyat bilgilerini assign edelim ve ürünü sepete ekleyin  Test 4
     //1-sepetteki ürünlerle eklediğimiz ürünlerin aynı olduğunu isim ve fiyat olarak doğrulayın
+
+
+    //İKİNCİ ODEV
+
+    //Ödev 1
+    //
+    //"http://webdriveruniversity.com/Actions" sayfasina gidin
+    //"Hover over Me First" kutusunun ustune gelin
+    //"Link 1" e tiklayin
+    //Popup mesajini yazdirin
+    //Popup'i tamam diyerek kapatin
+    //"Click and hold" kutusuna basili tutun
+    //"Click and hold" kutusunda cikan yaziyi yazdirin
+    //"Double click me" butonunu cift tiklayin
+    //
+
+    //Ödev 2
+    //"http://webdriveruniversity.com/IFrame/index.html" sayfasina gidin
+    //"Our Products" butonuna basin
+    //"Cameras product"i tiklayin
+    //Popup mesajini yazdirin
+    //"close" butonuna basin
+    //"WebdriverUniversity.com (IFrame)" linkini tiklayin
+    //"http://webdriveruniversity.com/index.html" adresine gittigini test edin
+    //
+
+    //Ödev 3
+    //
+    //1."http://webdriveruniversity.com/" adresine gidin
+    //2."Login Portal" a  kadar asagi inin
+    //3."Login Portal" a tiklayin  4.Diger window'a gecin
+    //5."username" ve  "password" kutularina deger yazdirin
+    //6."login" butonuna basin
+    //7.Popup'ta cikan yazinin "validation failed" oldugunu test edin  8.Ok diyerek Popup'i kapatin
+    //Ilk sayfaya geri donun
+    //Ilk sayfaya donuldugunu test edin
 }
